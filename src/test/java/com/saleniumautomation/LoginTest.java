d7fc2174940b985874f360f8235a3b34aaff17da
Vinayak <80584437+Vinayak2835@users.noreply.github.com>
2021-07-12 19:43:00 +0530

    Update LoginTest.java

--- src/test/java/com/saleniumautomation/LoginTest.java
+++ src/test/java/com/saleniumautomation/LoginTest.java
@@ -2,14 +2,11 @@ package com.saleniumautomation;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class LoginTest {
+	 
 	public static void main(String[] args) {
-		
 		//ChromeDriver driver = new ChromeDriver();
-		
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://www.saucedemo.com/");
